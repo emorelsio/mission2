cb2362c1177da168e817191a4fe7bbb3a0c39aa2
emorelsio <emilienmorel09@gmail.com>
2019-12-06 10:26:07 +0100

    premier commit

---
+++ extraction.php
@@ -0,0 +1,25 @@
+<?php
+$json = file_get_contents("patrimoine-fluvial-remarquable-des-bords-de-seine-et-de-marne0.json");
+
+$parsed_json = json_decode($json, true);
+
+$jsonFinal = [];
+
+for($i=0; $i<count($parsed_json); $i++){
+    $jsonFinal[$i]["commune"] = $parsed_json[$i]["fields"]["commune"];
+    $jsonFinal[$i]["identifian"] = $parsed_json[$i]["fields"]["identifian"];
+    $jsonFinal[$i]["elem_patri"] = $parsed_json[$i]["fields"]["elem_patri"];
+    if(empty($parsed_json[$i]["fields"]["elem_princ"])){
+        $jsonFinal[$i]["elem_princ"] = "Pas d'element principale";
+    }else{
+        $jsonFinal[$i]["elem_princ"] = $parsed_json[$i]["fields"]["elem_princ"];
+    }
+    
+
+}
+
+$encode_json = json_encode($jsonFinal);
+
+var_dump($encode_json);   
+
+?>
