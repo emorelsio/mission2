757b301552cd85d261d9c04c16e05a1d1e2fb8e2
emorelsio <emilienmorel09@gmail.com>
2019-12-10 15:27:45 +0100

    recherche commune réalisée

--- api.php
+++ api.php
@@ -4,5 +4,21 @@ header('Content-Type: application/json');
 include("db_connect.php");
 
 
+function requestCommune($departement)
+{
+    $resultat = [];
+    $bdd = connect();
 
-    
+    $sql = "SELECT DISTINCT commune FROM recherche WHERE identifiant LIKE '$departement%' ORDER BY commune";
+    //var_dump($sql);
+    $state = $bdd->query($sql);
+    $i = 0;
+    while($row = $state->fetch()) {
+        $resultat[$i] = (array)$row;
+        $i++;
+    }
+
+return $resultat;
+}
+
+$var = requestCommune(77);
